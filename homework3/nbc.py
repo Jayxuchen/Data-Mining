d56512a8ab2e3b263fcca7a0e9225d813442d353
Jason Chen <jayxuchen@gmail.com>
2017-03-08 02:43:06 -0500

    created vars

--- homework3/nbc.py
+++ homework3/nbc.py
@@ -1,2 +1,18 @@
-print("hello")
+import csv, sys
 
+if len(sys.argv) != 3:
+    print("invalid number of arguments : correct usage \"python nbc.py train-set.csv test-set.csv\"")
+    exit()
+def setData(filename):
+    trainingDataFilename =open(filename)
+    reader = csv.reader(trainingDataFilename)
+    headers = reader.next()
+    data = {}
+    for h in headers:
+        data[h] = []
+    for row in reader:
+        for h, v in zip(headers, row):
+            data[h].append(v)
+    return data
+trainData = setData('train-set1.csv')
+testData = setData('test-set1.csv')

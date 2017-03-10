55359026011775cabf621b3deb96ae02395020cf
Jason Chen <jayxuchen@gmail.com>
2017-03-09 19:41:53 -0500

    working on nbc algo

--- homework3/nbc.py
+++ homework3/nbc.py
@@ -1,5 +1,6 @@
 import csv, sys
 
+classLabel = 'goodForGroups'
 if len(sys.argv) != 3:
     print("invalid number of arguments : correct usage \"python nbc.py train-set.csv test-set.csv\"")
     exit()
@@ -19,3 +20,17 @@ def setData(filename):
     return data
 trainData = setData(sys.argv[1])
 testData = setData(sys.argv[2])
+target = open("test.txt", 'w')
+classPrior={'0','1'}
+for v in trainData['classLabel']:
+    if v == '0':
+        classPrior['0'] = classPrior['0']+1
+    else:
+        classPrior['1'] = classPrior['1']+1
+print(classPrior)
+for v in trainData.keys():
+    if v == classLabel:
+        continue
+    for i,k in enumerate(trainData[v]):
+        if(trainData[classLabel][i]=='1'):
+            continue

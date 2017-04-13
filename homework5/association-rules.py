7f6743b1d1c41b5e990cd2d8caf552fff178ca9b
jayxuchen <jayxuchen@gmail.com>
2017-04-12 23:42:56 -0400

    modified setDat

--- homework5/association-rules.py
+++ homework5/association-rules.py
@@ -6,9 +6,11 @@ def setData(filename):
     reader = csv.reader(trainingDataFilename)
     headers = reader.next()
     data = {}
-    sets {}
+    sets= {}
+    matrix = {}
     for h in headers:
         data[h] = []
+        matrix[h] = {}
         sets[h] = set()
     for row in reader:
         for h, v in zip(headers, row):
@@ -18,8 +20,25 @@ def setData(filename):
             else:
                 data[h].append(v)
                 sets[h].add(v)
-    return data
+    for k in matrix.keys():
+        for n in sets[k]:
+            matrix[k][n]=[]
+    # matrix['rating']['4'][]
+    for x in range(9337):
+        for k in matrix.keys():
+            for n in matrix[k].keys():
+                if data[k][x] == n:
+                    matrix[k][n].append(True)
+                else:
+                    matrix[k][n].append(False)
+    # print matrix['stars']['4']
+    return matrix
 
-if len(sys.argv) !=5:
+if len(sys.argv) !=4:
     print("invalid number of arguments : correct usage \"python association-rules.py yelp4.csv minsup minconf\"")
     exit()
+filename =  sys.argv[1]
+minsup = float(sys.argv[2])
+minconf = float(sys.argv[3])
+matrix = setData(filename)
+# setData()

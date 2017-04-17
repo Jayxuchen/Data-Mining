172cb8355cdf8101b3fc7392c5e7eaefdd2b948d
jayxuchen <jayxuchen@gmail.com>
2017-04-17 15:25:09 -0400

    added stuf

--- homework5/association-rules.py
+++ homework5/association-rules.py
@@ -1,7 +1,9 @@
 import csv,sys,random,math
 import numpy as np
 
+binaryAtts=['delivery','waiterService','caters']
 def setData(filename):
+    global binaryAtts
     trainingDataFilename =open(filename)
     reader = csv.reader(trainingDataFilename)
     headers = reader.next()
@@ -21,19 +23,36 @@ def setData(filename):
                 data[h].append(v)
                 sets[h].add(v)
     for k in matrix.keys():
-        for n in sets[k]:
-            matrix[k][n]=[]
+        if k in binaryAtts:
+            matrix[k] = []
+        else:
+            for n in sets[k]:
+                matrix[k][n]=[]
     # matrix['rating']['4'][]
     for x in range(9337):
         for k in matrix.keys():
-            for n in matrix[k].keys():
-                if data[k][x] == n:
-                    matrix[k][n].append(True)
+            if k in binaryAtts:
+                if data[k][x] == 'TRUE':
+                    matrix[k].append(True)
                 else:
-                    matrix[k][n].append(False)
+                    matrix[k].append(False)
+            else:
+                for n in matrix[k].keys():
+                    if data[k][x] == n:
+                        matrix[k][n].append(True)
+                    else:
+                        matrix[k][n].append(False)
     # print matrix['stars']['4']
     return matrix
-
+def getColumns(matrix):
+    count = 0
+    for k in matrix.keys():
+        if type(matrix[k]) is dict:
+            for n in matrix[k].keys():
+                count+=1
+        else:
+            count+=1
+    return count
 if len(sys.argv) !=4:
     print("invalid number of arguments : correct usage \"python association-rules.py yelp4.csv minsup minconf\"")
     exit()
@@ -41,4 +60,4 @@ filename =  sys.argv[1]
 minsup = float(sys.argv[2])
 minconf = float(sys.argv[3])
 matrix = setData(filename)
-# setData()
+# print getColumns(matrix)
